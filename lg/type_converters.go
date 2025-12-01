5d7881bf2c186cd1eff76c48059d0e30d82290d1
brezzgg <afonin1003@gmail.com>
2025-12-01 14:14:03 +0000

    lg: Improve type converter

--- lg/type_converters.go
+++ lg/type_converters.go
@@ -1,25 +1,25 @@
 package lg
 
-const FormatedErrorKey = "$formattedError"
-
 type DefaultTypeConverter struct{}
 
-func (DefaultTypeConverter) ConvAndPush(item any, push func(key string, val any)) {
-	if ctx, ok := item.(C); ok {
-		for key, value := range ctx {
-			push(key, value)
+func (c DefaultTypeConverter) ConvAndPush(item any, push func(key string, val any)) {
+	switch value := item.(type) {
+	case C:
+		c.convAndPushContext(value, push)
+	case error:
+		push("error", value.Error())
+	default:
+		push("arg", value)
+	}
+}
+
+func (c DefaultTypeConverter) convAndPushContext(context C, push func(key string, val any)) {
+	for key, val := range context {
+		switch v := val.(type) {
+		case error:
+			push(key, v.Error())
+		default:
+			push(key, v)
 		}
-	} else if err, ok := item.(error); ok {
-		push("error", err.Error())
-	} else if errS, ok := item.(E); ok {
-		v := ""
-		if errS.Err == nil {
-			v = "nil"
-		} else {
-			v = errS.Err.Error()
-		}
-		push("error", v)
-	} else if str, ok := item.(string); ok {
-		push("arg", str)
 	}
 }
